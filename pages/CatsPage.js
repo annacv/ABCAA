804606af21e2c1182c4bb18307ab8c9b7236d0c6
annacv <21217131+annacv@users.noreply.github.com>
2019-07-11 13:48:26 +0200

    [Cats][WIP] Some refactor, empty styles

--- pages/CatsPage.js
+++ pages/CatsPage.js
@@ -13,19 +13,16 @@ CatsPage.prototype.generate = async function() {
   
   await this.connectToAPI();
   this.elements = `
-    <header>
-      <h2>Supaaaar Amazing Black Cats Page!</h2>
-    </header>
     <section class="cards-container">
   `;
-  this.blackCats.forEach((cat) => {
-    let arrNames = ['Manel', 'Antonio', 'Eugènia', 'Eufràsia', 'Manolo', 'Antònia', 'Teta', 'Tata', 'Meritxell', 'Terca'];
-    let arrDescriptions = ['A classic roof\'s cat. Loves to look at the moon and hunt flies.', 'A kind of rare cat, practices swimming while eats chocolate.', 'Late at night this cat begins to jump searching for vampires.', 'A nice cat, friendly during the day, violent at night.', 'No one would think this cat speaks more than 5 languages, but it does.', 'This cat has the life all we dream of: just spends time eating and watching clouds.']
+  this.allCats.forEach((cat) => {
+    let arrNames = ['Manel', 'Antonio', 'Eugènia', 'Eufràsia', 'Manolo', 'Antònia', 'Teta', 'Tata', 'Meritxell', 'Terca', 'Topos', 'Misae', 'Xuxa', 'Shushi'];
+    let arrDescriptions = ['A classic roof\'s cat. Loves to look at the moon and hunt flies.', 'A kind of rare cat, practices swimming while eats chocolate.', 'Late at night this cat begins to jump searching for vampires.', 'A bipolar cat, friendly during the day, violent at night.', 'No one would think this cat speaks more than 5 languages, but it does.', 'This cat has the life all we dream of: just spends time eating and watching clouds.', 'This cat loves watermelon, but only when it\'s served as an icecream']
     this.elements += `
       <article>
         <div style="background-image: url('${cat.url}'); background-size: cover; background-position: center center; width: 200px; height: 200px;"></div>
-        <h3>${arrNames[Math.floor(Math.random()*10)]}</h3>
-        <p>${arrDescriptions[Math.floor(Math.random()*6)]}</p>
+        <h3 class="card-title">${arrNames[Math.floor(Math.random()*14)]}</h3>
+        <p class="card-description">${arrDescriptions[Math.floor(Math.random()*7)]}</p>
         
       </article>
     `
@@ -39,5 +36,5 @@ CatsPage.prototype.render = function() {
 }
 
 CatsPage.prototype.connectToAPI = async function() {
-  this.blackCats = await BlackCatsServiceInstance.getCatslist();
+  this.allCats = await AllCatsServiceInstance.getCatslist();
 }
