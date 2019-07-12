0701c92757378f8716c166198fecd65f00fd8848
annacv <21217131+annacv@users.noreply.github.com>
2019-07-12 09:26:04 +0200

    .st

--- components/Loading.js
+++ components/Loading.js
@@ -7,10 +7,10 @@ function Loading(parentElement) {
 
 Loading.prototype.generate = function() {
   this.elements = `
-    <main class="loader container">
+    <div class="loader container">
       <p class="loader-message"> >> Wait! Cats are coming from the ceilings...</p>
       <img class= "loader-gif" src="https://media.giphy.com/media/kLka7AlsedWZq/giphy.gif" alt="Cat Flying Cats GIF" style="width: 100%; height: auto;">
-    </main>
+    </div>
     `
   this.render();
 }
