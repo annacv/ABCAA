383189d7d44c888222ac9e950e7fb27598c9ecc8
annacv <21217131+annacv@users.noreply.github.com>
2019-07-15 20:36:50 +0200

    [RF] Landing

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,10 +1,34 @@
 'use strict'
 
-function LandingPage(parentElement) {
-  this.parentElement = parentElement;
-  this.elements = null;
+class LandingPage {
+  constructor (parentElement) {
+    this.parentElement = parentElement;
+    this.elements = null;
+  }
+
+  generate() {
+    this.elements = `
+    <section class="landing-intro">
+      <div class="intro-section">
+        <h2 class="intro-title">ACAB</h2>
+        <p class="intro-description">Presents</p>
+      </div>
+      <div class="intro-brand">
+        <h1 class="brand-logo"><img src="./assets/images/tindercats.svg" alt="Tindercats logo" class="brand-image"></h1>
+        <a class="intro-cta" href="#0" url="/miolouu">Miolouu</a>
+        <p class="intro-description acab">allcatsareboilerplates</p>
+      <div>
+    </section>
+    `; 
+    this.render();
+  }
+
+  render() {
+    this.parentElement.innerHTML = this.elements;
+  }
 }
 
+/*
 LandingPage.prototype.generate = function() {
   this.elements = `
     <section class="landing-intro">
@@ -25,3 +49,4 @@ LandingPage.prototype.generate = function() {
 LandingPage.prototype.render = function() {
   this.parentElement.innerHTML = this.elements;
 }
+*/
