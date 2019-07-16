a4d5f0c63aa9c84e983d16445da33306838d7621
annacv <21217131+annacv@users.noreply.github.com>
2019-07-16 08:15:40 +0200

    [ST] Fix loader section

--- components/Loading.js
+++ components/Loading.js
@@ -7,10 +7,12 @@ function Loading(parentElement) {
 
 Loading.prototype.generate = function() {
   this.elements = `
-    <div class="loader container">
-      <p class="loader-message"> >> Wait! Cats are coming from the ceilings...</p>
-      <img class= "loader-gif" src="https://media.giphy.com/media/kLka7AlsedWZq/giphy.gif" alt="Cat Flying Cats GIF" style="width: 100%; height: auto;">
-    </div>
+    <section class="loader">
+      <div class="loader-container">
+        <p class="loader-message"> >> Wait! Cats are coming from the ceilings...</p>
+        <img class= "loader-gif" src="https://media.giphy.com/media/kLka7AlsedWZq/giphy.gif" alt="Cat Flying Cats GIF" style="width: 100%; height: auto;">
+      </div>
+    </section>
     `
   this.render();
 }
